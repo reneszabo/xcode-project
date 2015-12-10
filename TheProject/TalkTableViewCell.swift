d0e8cbe5d1ba6ec585972eee4bb5230d4778511a
Ramirez Szabo Rene Roberto <n01067860@l209-n-024.mediastudies.ca>
2015-12-10 12:43:41 -0500

    Add TableView

---
+++ TheProject/TalkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TalkTableViewCell.swift
+//  TheProject
+//
+//  Created by Ramirez Szabo Rene Roberto on 12/10/15.
+//  Copyright (c) 2015 Ramirez and Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class TalkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

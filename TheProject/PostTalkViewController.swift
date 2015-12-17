3b527bc143cda19711aaa54c06d9c7fe1ad9c107
Rene Ramirez <asus@Renes-MacBook-Pro.local>
2015-12-16 19:18:06 -0800

    Add Post Talk View

---
+++ TheProject/PostTalkViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PostTalkViewController.swift
+//  TheProject
+//
+//  Created by Rene Ramirez on 2015-12-16.
+//  Copyright © 2015 Ramirez and Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class PostTalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  override func viewWillAppear(animated: Bool) {
+    self.navigationController?.navigationBarHidden = true;
+    self.navigationController?.navigationItem.backBarButtonItem?.enabled = true
+    (self.navigationController as! NavigationViewController).showMenu()
+  }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
